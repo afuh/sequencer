12f89aa11075e0bcce4b760f177f318fa58e1ed8
Axel <axelfuh@gmail.com>
2017-06-24 15:15:52 +0200

    Indent

--- src/modules/build.js
+++ src/modules/build.js
@@ -18,47 +18,47 @@ let square
 export {row, columns, parent, play, stop, eightN }
 
 export const seq = {
-      init() {
-          seq.buildSequencer();
-          seq.bindEvent();
-          seq.fadeIn();
-      },
-      buildSequencer() {
-          //build rows
-          for (let i = 9; i >= 0; i--) {
-            matrix.prepend(`<div class='row r${i}'></div>`);
-          }
-          //build columns
-          row.map((x) => {
-            for (let i = columns; i > 0; i--) {
-              $(x).prepend(`<div class="square"></div>`);
-            }
-          });
-          square = parent.find(".square")
-      },
-      bindEvent() {
-          play.click(press.play);
-          stop.click(press.stop);
-          reset.click(seq.reset);
-          square.mousedown(seq.paint);
-          seq.paintDrag();
-      },
-      paint() {
-          $(this).toggleClass("square-active");
-      },
-      paintDrag() {
-          doc.mousedown(() => {
-            matrix.on("mouseover", ".square", function(){
-              $(this).toggleClass("square-active");
-            });
-          }).mouseup(() => {
-              matrix.off("mouseover");
-            });
-      },
-      reset() {
-          square.removeClass("square-active");
-      },
-      fadeIn(){
-        parent.fadeIn(600);
+  init() {
+      seq.buildSequencer();
+      seq.bindEvent();
+      seq.fadeIn();
+  },
+  buildSequencer() {
+      //build rows
+      for (let i = 9; i >= 0; i--) {
+        matrix.prepend(`<div class='row r${i}'></div>`);
       }
+      //build columns
+      row.map((x) => {
+        for (let i = columns; i > 0; i--) {
+          $(x).prepend(`<div class="square"></div>`);
+        }
+      });
+      square = parent.find(".square")
+  },
+  bindEvent() {
+      play.click(press.play);
+      stop.click(press.stop);
+      reset.click(seq.reset);
+      square.mousedown(seq.paint);
+      seq.paintDrag();
+  },
+  paint() {
+      $(this).toggleClass("square-active");
+  },
+  paintDrag() {
+      doc.mousedown(() => {
+        matrix.on("mouseover", ".square", function(){
+          $(this).toggleClass("square-active");
+        });
+      }).mouseup(() => {
+          matrix.off("mouseover");
+        });
+  },
+  reset() {
+      square.removeClass("square-active");
+  },
+  fadeIn(){
+    parent.fadeIn(600);
+  }
 };
--- src/modules/run.js
+++ src/modules/run.js
@@ -6,57 +6,58 @@ let playing = false
 
 //Run the sequencer
 const press = (function(){
-      let count = 1;
-      let init;
-      const button = {};
+  let count = 1;
+  let init;
+  const button = {};
 
-      button.play = function() { /// PLAY ///
-          play.hide();
-          stop.show();
+  button.play = function() { /// PLAY ///
+    play.hide();
+    stop.show();
 
-          const drawColumn = (n) => {
-            const loc = ` div:nth-child(${columns}n+${n})`
-            const col = parent.find(`.row${loc}`);
+    const drawColumn = (n) => {
+      const loc = ` div:nth-child(${columns}n+${n})`
+      const col = parent.find(`.row${loc}`);
 
-            col.addClass("colorOn").delay(eightN).queue(function(){
-              $(this).removeClass("colorOn").dequeue();
-            });
-            //When the orange column meet an orange square make sound
-            const map = (col) => {
-              col.map((col, i) => {
-                if ($(col + loc).hasClass("square-active")) {
-                  note["r" + i].play();
-                }
-              });
-            };
-            map(row);
-          };
+      col.addClass("colorOn").delay(eightN).queue(function(){
+        $(this).removeClass("colorOn").dequeue();
+      });
+      //When the orange column meet an orange square make sound
+      const map = (col) => {
+        col.map((col, i) => {
+          if ($(col + loc).hasClass("square-active")) {
+            note["r" + i].play();
+          }
+        });
+      };
+      map(row);
+    };
 
-          init = setInterval(() => {
-            if (count >= 1 && count <= columns -1) {
-              drawColumn(count);
-              count++;
-            }
-            else {
-              drawColumn(columns);
-              count = 1;
-            }
-          }, eightN); /**** TEMPO ****/
-        };
+    init = setInterval(() => {
+      if (count >= 1 && count <= columns -1) {
+        drawColumn(count);
+        count++;
+      }
+      else {
+        drawColumn(columns);
+        count = 1;
+      }
+    }, eightN); /**** TEMPO ****/
+  };
 
-      button.stop = function() { /// STOP ///
-          stop.hide();
-          play.show();
+  button.stop = function() { /// STOP ///
+    stop.hide();
+    play.show();
 
-          clearInterval(init);
-          };
-          return button;
+    clearInterval(init);
+    };
+    return button;
 })();
 
 
 // Space bar stop/play
 $(window).keydown(e => {
   if (e.keyCode === 32) {
+    e.preventDefault()
     playing = !playing
 
     if (playing) stop.click()
